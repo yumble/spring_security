450862578003cd50ef3d37302645afa726143254
yumble <hnjn1101@gmail.com>
2022-11-09 13:05:20 +0900

    7강 구글 회원 정보 받아오기

--- src/main/java/com/example/security1/Config/SecurityConfig.java
+++ src/main/java/com/example/security1/Config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.security1.Config;
 
+import com.example.security1.Config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -13,6 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // 스프링 시큐리티 필터(securityconfig 에서 등록한 필터 ) 가 스프링 필터체인에 등록이 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // Bean 으로 등록시 해당 메서드의 리턴되는 obj를 IOC 로 등록해준다.
     @Bean
@@ -35,7 +40,9 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
 
         return http.build();
 
---
+++ src/main/java/com/example/security1/Config/oauth/PrincipalOauth2UserService.java
@@ -0,0 +1,22 @@
+package com.example.security1.Config.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+//        System.out.println("userRequest = " + userRequest);
+//        System.out.println("userRequest.getClientRegistration() = " + userRequest.getClientRegistration());
+//        System.out.println("userRequest.getAccessToken() = " + userRequest.getAccessToken());
+//        System.out.println("userRequest.get = " + userRequest.getAccessToken().getTokenValue());
+//        System.out.println("super.loadUser(userRequest).getAttributes() = " + super.loadUser(userRequest).getAttributes());
+        return super.loadUser(userRequest);
+    }
+}
